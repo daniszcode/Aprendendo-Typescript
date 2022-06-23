c938328e435d6937badd216b5b5bb612bcaef2d2
daniszcode <daniele.marques@creditas.com>
2022-06-23 11:18:08 -0300

    feat: new knowledge: type tuple

---
+++ src/Type-Tuple/index.ts
@@ -0,0 +1,9 @@
+const digimon: [string, number, string?, ...string[]] = ['agumon', 1];
+
+console.log(digimon);
+
+const digimonDigivolve: ReadonlyArray<string> = ['greymon'];
+
+console.log(digimonDigivolve);
+
+//imultavel
