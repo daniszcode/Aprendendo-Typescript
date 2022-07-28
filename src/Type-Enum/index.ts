74b7d352039fd9970741acd31045afe3bd5282ef
daniszcode <daniele.marques@creditas.com>
2022-07-28 17:20:00 -0300

    feat: create archive of enum type

---
+++ src/Type-Enum/index.ts
@@ -0,0 +1,7 @@
+enum AmountInputTestExemples {
+  LABEL = 'TOP_UP_AMOUNT_LABEL',
+  INPUT = 'TOP_UP_AMOUNT_INPUT',
+  SKELETON = 'TOP_UP_AMOUNT_SKELETON',
+}
+
+console.log(AmountInputTestExemples.LABEL);
