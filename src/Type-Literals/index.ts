075c11c944a5dccd78bada6a3e0aa2c8f6065a72
daniszcode <daniele.marques@creditas.com>
2022-08-22 19:55:04 -0300

    fix: new knowledge is open, type-literals and union-types

---
+++ src/Type-Literals/index.ts
@@ -0,0 +1,11 @@
+function escolheCasa(
+  Casas: 'Grifinoria' | 'Lufa-Lufa' | 'Sonserina' | 'Corvinal',
+): string {
+  return Casas;
+}
+
+console.log(
+  'Funções literais mostram seu tipo de forma declarativa, implicita e literal, a proposito minha casa só poderia ser: ',
+  escolheCasa('Corvinal'),
+  escolheCasa(''),
+);
--- src/Union-Types/index.ts
+++ src/Union-Types/index.ts
@@ -1,10 +1,11 @@
-function AddOrConcat(a: number | string, b: number | string) {
+function AddOrConcat(a: number | string, b: number | string): number | string {
   if (typeof a === 'number' && typeof b === 'number') return a + b;
-  if (typeof a === 'string' && typeof b === 'string') return a + b;
+  // if (typeof a === 'string' && typeof b === 'string') return a + b;
+  return `${a} ${b}`;
 }
 
 console.log(AddOrConcat(5, 's'));
 console.log(AddOrConcat('5', 's'));
 console.log(AddOrConcat(5, 5));
 
-//union-types refere-se ao caracter | //
+//union-types refere-se ao caracter |, usa-se quando é necessario utilizar varios tipos em um mesmo contexto //
